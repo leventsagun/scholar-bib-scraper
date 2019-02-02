a4acd85e3a0c1076dc4ac57ce752ca014d6a2e2a
Levent Sagun <leventsagun@gmail.com>
2019-02-02 01:53:28 -0800

    fix saving the first page, add comment on final step

--- scholar-bib-scraper.py
+++ scholar-bib-scraper.py
@@ -7,6 +7,8 @@ from selenium.webdriver.support import ui
 
 
 def get_entries_on_file(driver, log_file='links.txt', bib_file='refs.bib'):
+    with open(log_file, 'a') as f: # for logging the current page
+        f.write(driver.current_url + '\n')
     # all these sleeps may not be really necessary
     time.sleep(1)
     select_all = wait.until(lambda driver: driver.find_element_by_id('gs_res_ab_xall'))
@@ -26,8 +28,6 @@ def get_entries_on_file(driver, log_file='links.txt', bib_file='refs.bib'):
     text = body.text 
     with open(bib_file, 'a') as f: # keep appending the bibtex entries to file
         f.write(text + '\n\n')
-    with open(log_file, 'a') as f: # for logging
-        f.write(driver.current_url + '\n')
 
 
 if __name__ == '__main__':
@@ -45,13 +45,16 @@ if __name__ == '__main__':
     library = wait.until(lambda driver: driver.find_element_by_partial_link_text('library'))
     time.sleep(1)
     library.click()
-
+    time.sleep(1)
+    get_entries_on_file(driver) # get the 10 entries at the first page
     time.sleep(90) # this is where I had to solve reCAPTCHA
 
     for i in range(1, 500): 
         # this loop breaks with some error after all done
+        # after the loop there will be total%10 more articles left
         time.sleep(1)
         link = 'https://scholar.google.com/scholar?start={}&hl=en&as_sdt=0,5&scilib=1'.format(10*i)
         driver.get(link); # alt method: find and click NEXT until no longer can
         get_entries_on_file(driver)
+
 
